2c85b736f284c30e83a911b02564522477c9d09c
Parth <phalke270@gmail.com>
2021-09-22 20:04:19 +0530

    Deleted the main branch and added sydney branch

--- src/components/Compete/Compete.js
+++ src/components/Compete/Compete.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Menu from '../Navbar/Menu';
+import axios from 'axios';
+import useEffect from "react";
+
+let gg;
+function getData(tourdata){
+    gg = tourdata
+}
+axios.get("http://localhost:9002/compete").then(res => getData(res.data))
+        
 
 const Compete = ({updateUser}) => {
     
+        
+        console.log(gg)
+   
+
         return (
             <div>
                 <div>
                     <Menu updateUser={updateUser}/>
                 </div>
-
+                <div>
+                <ul>
+                { }
+            </ul>
+                </div>
             </div>
         )
     }
