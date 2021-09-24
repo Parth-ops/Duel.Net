1e35c23978d85c5249193072c7fca25dce7483c0
aerodeval <67939367+aerodeval@users.noreply.github.com>
2021-09-24 10:01:38 +0530

    modified tiles css

--- src/components/tiles/Tiles.js
+++ src/components/tiles/Tiles.js
@@ -3,24 +3,33 @@ import Card from "../card/cards";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "../Navbar/Menu.js";
 import "./Tiles.css"
+import val from './valo.jpg'
  const Tiles = (props)=>{
     return(
         <div className="cerd">
-        <h5>Createdby:{props.cb}
-        <br/>
-        no. of team:{props.noft}
-        <br/>
-        system:{props.sys}
-        <br/>
-        title:{props.tn}
-        </h5>
-        </div>
+   
+ <div className="bracket">
+            <div className="tourimg">
+           <img  src={val} />
+           </div>
+         <div className="text">
+                <p>
+                Createdby:{props.cb}
+                </p>
+                <p>
+                no. of team:{props.noft}
+               </p>
+               <p>
+                system:{props.sys}
+               </p>
+               <p>
+                title:{props.tn}
+                </p>
+         </div>
         
-
+ </div>
+        </div>
     )
-
-
-
 
 
 
