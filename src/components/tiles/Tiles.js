09d53f3affcf6ce0f41a0f3b73df1480119c33fd
aldrich2002 <67966175+aldrich2002@users.noreply.github.com>
2021-09-26 22:22:08 +0530

    Add files via upload

--- src/components/tiles/Tiles.js
+++ src/components/tiles/Tiles.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState,useEffect, } from "react";
 import Card from "../card/cards";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "../Navbar/Menu.js";
 import "./Tiles.css"
-import val from './valo.jpg'
+import val from './valo.jpg';
+import { useHistory } from "react-router-dom";
+
+
  const Tiles = (props)=>{
+       const history = useHistory()
+       
+
+       const updateT=()=>{
+              const deezp= props
+              history.push('/Regt',props)
+              
+          
+          
+          
+            }     
+
+
+     
+
+     
+
+ 
+
+       // useEffect(() => {
+       //        setd(JSON.parse(localStorage.getItem("tdata")))
+       //      }, [])
+       
+        
     return(
-        <div className="cerd">
+        <div className="cerd ">
    
- <div className="bracket">
+ <div className="bracket"  >
             <div className="tourimg">
            <img  src={val} />
            </div>
@@ -24,10 +51,15 @@ import val from './valo.jpg'
                </p>
                <p>
                 title:{props.tn}
+                
                 </p>
+                <button  onClick={updateT}  > reg</button>
          </div>
+
+
         
  </div>
+
         </div>
     )
 
