f085327313331a2774f4725cae406ebfeba6e142
Parth <phalke270@gmail.com>
2021-10-18 10:25:18 +0530

    Integrated riot API using node package. Sucessfully fetched leaderboards and content data.

--- src/components/Leaderboards/Leaderboard.js
+++ src/components/Leaderboards/Leaderboard.js
@@ -1,9 +1,15 @@
+import axios from "axios";
 import React from "react";
-
+import { useEffect } from "react";
 const Leaderboard = ({updateUser}) =>{
+    useEffect(()=>{
+        axios.post('http://localhost:9002/leaderboard').then(res=>{
+            console.log(res.data.content_data)
+        })
+    }, [])
     return(
         <div>
-            Leaderboard
+            
         </div>
     )
 }
